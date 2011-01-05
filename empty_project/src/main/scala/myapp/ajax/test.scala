73c54f7ee2052ad333d929227c05d59d8d896efd
Jan Tuitman <jan.tuitman@gmail.com>
2011-01-05 14:04:18 +0100

    test mongodb implementation.

--- empty_project/src/main/scala/myapp/ajax/test.scala
+++ empty_project/src/main/scala/myapp/ajax/test.scala
@@ -1,20 +1,67 @@
 package myapp.ajax;
 import org.tuitman.statelesswf.AjaxClass;
-import org.tuitman.statelesswf.AjaxMethod;
+import net.liftweb.mongodb._;
+import net.liftweb.json.JsonDSL._;
+import com.mongodb.DBObject;
+import net.liftweb.json.JsonAST.JObject;
+
 
 
 case class UserAddress(street :String);
-case class User(name : String, email : String,list : List[UserAddress]);
+case class User(_id: String, name : String, email : String,list : List[UserAddress]) extends MongoDocument[User] {
+	def meta = User;
+};
+
+object User extends MongoDocumentMeta[User] {
+  override def collectionName = "myusers"	
+}
 
 class Test extends AjaxClass {
+
+	MongoDB.defineDb(DefaultMongoIdentifier, MongoAddress(MongoHost("localhost", 27017), "test"))
 		
   
 val addUser = ajaxMethod[User,User] { 
-	( u : User) =>
-	println("User name "+u.name);
+	( u : User) => u match {
+		case User(id,name,email,list) => {
+			User(null,name,email,list).save ;
+		}
+	}
 	u;
 }  
 
+
+val findUserByName = ajaxMethod[User,List[User]] {
+	( u : User) =>
+
+	User.findAll("name" -> u.name);
+}
+
+val allUsers = ajaxMethod[User,List[User]] {
+	( u : User) =>
+    
+    User.findAll(JObject(Nil));	
+}
+
+
+val updateUser = ajaxMethod[User,User] {
+	( u : User) =>
+    
+    User.update("name" -> u.name,u );
+    u;
+	
+}
+
+
+val deleteUser= ajaxMethod[User,User] {
+	( u : User) =>
+    
+    User.find("name" -> u.name) match {
+	   case Some(user) => user.delete;
+	   case _ => ; 
+    }
+    u;	
+}
 
 
 
